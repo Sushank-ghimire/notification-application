7c10e0924355e85e551e62565fbcbc48f4ba02da
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-07-10 16:48:19 +0545

    add error handling middleware to express app; log errors and send standardized error responses

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import http from "node:http";
 import { configDotenv } from "dotenv";
+import { sendError } from "./utils";
 
 configDotenv();
 
@@ -12,6 +13,11 @@ app.use(express.json()); // For Json parsing
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Server is running!" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  sendError(res, "Internal Server Error", 500, err);
 });
 
 export default server;
