cc5179be4276a1142f398b77e68c6db9c71a5dfb
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-07-10 17:19:23 +0545

    add user notification routes and controller methods; implement CRUD operations for notifications

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import http from "node:http";
 import { configDotenv } from "dotenv";
 import { sendError } from "./utils";
+import notificationRoutes from "./routes/notification.route";
 
 configDotenv();
 
@@ -15,9 +16,13 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Server is running!" });
 });
 
+// Error handling from the asyncHandler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   sendError(res, "Internal Server Error", 500, err);
 });
+
+// User notification routes
+app.use("/api/v1/users/notification", notificationRoutes);
 
 export default server;
---
+++ backend/app/routes/notification.route.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  getNotifications,
+  markAsRead,
+  unreadNotification,
+} from "../controllers/NotificationControllers";
+
+const notificationRoutes = Router();
+
+// GET all notifications for a user (userId from body)
+notificationRoutes.get("/", getNotifications);
+
+// PATCH - Mark a specific notification as read
+notificationRoutes.patch("/:id/read", markAsRead);
+
+// PATCH - Mark a specific notification as unread
+notificationRoutes.patch("/:id/unread", unreadNotification);
+
+// POST - Create a new notification (backend/admin trigger)
+// notificationRoutes.post("/", createNotification);
+
+export default notificationRoutes;
